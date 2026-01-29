11e6ba35bded3c9e1f08ea1966fa2fe18388d09e
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2026-01-29 17:03:37 +0100

    send list of users into the dashboard page

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,17 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function (Request $request) {
-
-    $user = auth()->user();
-    $search = $request->input('search');
-
-    $targetRole = ($user->role === 'recruteur') ? 'chercheur' : 'recruteur';
-
-    $users = User::where('role', $targetRole)
-        ->when($search, function ($query) use ($search) {
-            $query->where(function ($q) use ($search) {
-                $q->where('name', 'ILIKE', "%{$search}%")
-                  ->orWhereHas('profile', function ($profileQuery) use ($search) {
-                      $profileQuery->where('specialty', 'ILIKE', "%{$search}%");
-                  });
-            });
-        })
-        ->with('profile')
-        ->get();
+    $users = User::all();
     return view('dashboard', compact('users'));
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/profile/{id}', function ($id) {
+    $user = User::with('profile')->findOrFail($id);
+    return view('profile.show', compact('user'));
+})->middleware(['auth'])->name('profile.show');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
