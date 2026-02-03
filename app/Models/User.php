7e17ba8abc9e7a612bd1db2ce7d81f4eaae4ebc8
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2026-02-03 12:19:23 +0100

    i added the liveware to the application

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public function profile() {
         return $this->hasOne(Profile::class);
-    }
+    }   
 
     public function jobs() {
         return $this->hasMany(Job::class);
