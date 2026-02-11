d02309b2d8f0371b4848fffc2f6d23925f74d898
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-02-11 23:30:48 +0100

    creer event userStatusUpdate pour changer etat de user

---
+++ app/Events/UserStatusUpdated.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserStatusUpdated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $userId;
+    public $status; 
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($userId, $status)
+    {
+        $this->userId = $userId;
+        $this->status = $status;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+         return new Channel('user-status.' . $this->userId); 
+    }
+
+     public function broadcastAs()
+    {
+        return 'user.status.updated';
+    }
+
+
+    public function broadcastWith()
+    {
+        return [
+            'userId' => $this->userId,
+            'status' => $this->status,
+        ];
+    }
+}
+
+
+
+
+
+
+
